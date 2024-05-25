2d13d6ac5884cb45ec75542cc74b16f26bd9eace
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-24 23:23:22 -0300

    Storybook Header

---
+++ src/components/Header/index.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import index from './index.tsx'
+
+const meta: Meta<typeof index> = {
+    component: index,
+  };
+
+export default meta
+type Story = StoryObj<typeof index>
+
+export const FistTest:Story = {
+    
+}
