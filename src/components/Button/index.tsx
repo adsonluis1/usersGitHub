bc55867332e3a07a93b33958811da0f74d884ca2
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-24 23:21:19 -0300

    Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import * as S from './styled'
+
+interface Ibutton {
+  value:string
+  dark:boolean
+  func:(url?:string)=>void
+}
+
+const Button = ({value,func,dark=false }:Ibutton) => {
+  return (
+    <S.button onClick={()=>{func()}} $dark={dark}>{value}</S.button>
+  )
+}
+
+export default Button
