2e6851ddc7da2bed7a3cd3beca0f1a72cc689e56
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-24 23:24:25 -0300

    Home

---
+++ src/components/home/index.tsx
@@ -0,0 +1,14 @@
+import CardPerfils from '../CardPerfils'
+import * as S from './styled'
+
+const Home = () => {
+  return (
+    <S.main>
+        <S.section>
+          <CardPerfils />
+        </S.section>
+    </S.main>
+  )
+}
+
+export default Home
