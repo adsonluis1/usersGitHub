e41477c516dd9362433ed51c590f629194dbc979
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-24 23:22:37 -0300

    styled CardPerfils

---
+++ src/components/CardPerfils/styled.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const headererror = styled.h2`
+    font-size: 1.3em;
+    color: white;
+`
+
+export const divError = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 10px;
+    width: 100vw;
+    height: 100%;
+
+`
+
+export const div = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: black;
+    color: white;
+    border-radius: 10px 10px 0px 0px;
+    gap: 20px;
+    margin-top: 20px;
+    padding: 20px;
+    width: 200px;
+`
+
+export const img = styled.img`
+    border-radius: 50%;
+    width: 150px;
+    height: 150px;
+`
+
+export const h3 = styled.h3`
+    font-size: 1.3em;
+`
