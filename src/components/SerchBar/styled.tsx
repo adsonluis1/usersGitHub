4c2f3e866aa1147480ded196a3d727e2361c93c6
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-24 23:26:38 -0300

    styled SearchBar

---
+++ src/components/SerchBar/styled.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const div = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+`
+
+export const input = styled.input`
+    border-radius: 5px;
+    border: none;
+    width: clamp(170px,10vw,400px);
+    padding: 5px;
+`
+
