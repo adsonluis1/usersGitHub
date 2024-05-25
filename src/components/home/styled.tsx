51864f140e228e397180648b9d577cf33ffed292
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-24 23:24:51 -0300

    styled Home

---
+++ src/components/home/styled.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const main = styled.main`
+    width: 100%;
+    min-height: 100%;
+    background-color: rgb(26, 35, 47);
+`
+
+export const section = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
+    height: 70%;
+    flex-wrap: wrap;
+`
