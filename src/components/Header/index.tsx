196d5d38dc0b6a27a7486ed4fa62e199ff496159
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-25 22:39:45 -0300

    Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,13 @@
+import { useNavigate } from 'react-router-dom'
 import SearchBar from '../SerchBar'
 import * as S from './styled'
 
 
 const Header = () => {
+  const navigate = useNavigate()
   return (
     <S.header>
-        <S.h1>Users<span>GitHub</span></S.h1>
+        <S.h1 onClick={()=>{navigate('/')}}>Users<span>GitHub</span></S.h1>
         <SearchBar />          
     </S.header>
   )
