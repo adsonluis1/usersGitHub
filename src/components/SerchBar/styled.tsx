834c8e7cd501c10e51a1d106d3e7ebe9002a7227
adsonluis1 <dev.adsonluis@gmail.com>
2024-05-25 22:41:16 -0300

    styled SerchBar

--- src/components/SerchBar/styled.tsx
+++ src/components/SerchBar/styled.tsx
@@ -4,12 +4,12 @@ export const div = styled.div`
     display: flex;
     align-items: center;
     gap: 5px;
-`
 
-export const input = styled.input`
-    border-radius: 5px;
-    border: none;
-    width: clamp(170px,10vw,400px);
-    padding: 5px;
+    & input {
+        border-radius: 5px;
+        border: none;
+        width: clamp(170px,10vw,400px);
+        padding: 5px;
+    }
 `
 
